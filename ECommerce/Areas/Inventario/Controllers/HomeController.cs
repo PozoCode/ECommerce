092beea04ec61d32748a01c20a3afe0c9bf23530
CristianPozo <35204280+PozoCode@users.noreply.github.com>
2023-09-21 06:57:03 -0500

    Se configura el Área de Inventarios

--- ECommerce/Areas/Inventario/Controllers/HomeController.cs
+++ ECommerce/Areas/Inventario/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace ECommerce.Controllers
+namespace ECommerce.Areas.Inventario.Controllers
 {
+    [Area("Inventario")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
