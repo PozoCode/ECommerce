3e6477b66b529c055cfa79caf7d467879f92043b
CristianPozo <35204280+PozoCode@users.noreply.github.com>
2023-09-23 17:20:57 -0500

    Se añade NegocioController y vista Index

---
+++ ECommerce/Areas/Admin/Controllers/NegocioController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.AccesoDatos.Repositorio.IRepositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NegocioController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public NegocioController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        #region ObtenerTodos
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var negocios = await _unidadTrabajo.Negocio.GetAll();
+
+            return View(negocios);
+        }
+        #endregion
+
+    }
+}
